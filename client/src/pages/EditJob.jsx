487c2e31b0b4ec91af8ef2a7ab6e86724d4e87f5
Emman <eman.gascon@gmai.com>
2024-11-30 08:11:37 +0800

    Nov 30 Version

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constant';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
 
-const EditJob = () => {
-  return <h1>Edit Job</h1>
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error.response.data.msg);
+    return redirect('/dashboard/all-jobs');
+  }
+};
+export const action = async () => {
+  return null;
 };
 
-export default EditJob
+const EditJob = () => {
+  const params = useParams();
+  console.log(params);
+  const { job } = useLoaderData();
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  return <h1>EditJob Page</h1>;
+};
+export default EditJob;
